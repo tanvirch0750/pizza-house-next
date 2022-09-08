3c3128b93fdadffd5d978d5bf7d8c3b1a9dc97ba
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-09 02:03:18 +0600

    connect to mongodb and implemented post request for adding pizza

---
+++ pages/api/products/index.js
@@ -0,0 +1,21 @@
+import Product from '../../../models/Product';
+import dbConnect from '../../../utils/mongo';
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  if (method === 'GET') {
+  }
+
+  if (method === 'POST') {
+    try {
+      dbConnect();
+
+      const product = await Product.create(req.body);
+
+      res.status(201).json(product);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
